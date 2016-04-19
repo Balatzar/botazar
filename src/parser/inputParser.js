8323c424f9185d76b6417189cd8d163c2201bfbf
balatzar <bataprod@gmail.com>
2016-04-19 17:38:57 +0200

    create json parser

---
+++ src/parser/inputParser.js
@@ -0,0 +1,5 @@
+const apps = require("./jsonParser.js")();
+
+module.exports = function(input) {
+  
+}
---
+++ src/parser/jsonParser.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+
+module.exports = function() {
+  "use strict";
+  let apps = [];
+  const path = "../apps";
+  const dirs = fs.readdirSync(path);
+
+  for (let i = 0; i < dirs.length; i += 1) {
+    try {
+      let commands = fs.readFileSync(path + "/" + dirs[i] + "/commands.json", "utf8");
+      apps.push(JSON.parse(commands));
+    } catch (e) {
+      console.log("/!\\ " + dirs[i] + " app has no commands.json");
+    }
+  }
+  return apps;
+};
