06fa1dbca28a3ecd9ed874b90b8ba6e8b7e1b0cd
balatzar <bataprod@gmail.com>
2016-04-20 10:35:56 +0200

    add second test

--- src/apps/feedback/modules/helpModule.js
+++ src/apps/feedback/modules/helpModule.js
@@ -1,6 +1,6 @@
 module.exports = function() {
   "use strict";
-  const help =  "This app is used for sending feedback to the botmaker!\n" +
+  const help =  "This app is to send feedback to the botmaker!\n" +
               "You can you use it with the syntax `feedback [type] [msg]\n" +
               "They are 3 types : idea, bug, msg\n" +
               "Example : `/balt feedback idea 'create an app to remind me to commit every now and then'`";
--- src/services/slack/slack.js
+++ src/services/slack/slack.js
@@ -1,3 +1,4 @@
 const inputParser = require("../../parser/inputParser");
 
-console.log(inputParser("fdb -h"))
+console.log(inputParser("fdb -h"));
+console.log(inputParser("feedback -help"));
