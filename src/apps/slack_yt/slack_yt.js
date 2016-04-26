b574dc1e590f4fc8a055cca993ee213fc31f8c54
balatzar <bataprod@gmail.com>
2016-04-26 10:04:25 +0200

    save

--- src/apps/slack_yt/slack_yt.js
+++ src/apps/slack_yt/slack_yt.js
@@ -1,7 +1,33 @@
-module.exports = function(input, message, out) {
+const youtube = require("youtube-api");
+const opn = require("opn");
+
+module.exports = function(arrInput, command, funcOut) {
   "use strict";
-  const reg = new RegExp("^(<https\\:\\/\\/)?(www\\.youtube\\.com|youtu\\.?be)\\/.+$");
-  let text = input.text.split(" ");
-  console.log(input.command)
-  text.forEach(e => console.log(e));
-}
+
+  const oauth = youtube.authenticate({
+    type: "oauth",
+    client_id: process.env.YOUTUBE_CLIENT_ID,
+    client_secret: process.env.YOUTUBE_CLIENT_SECRET
+  });
+
+  opn(oauth.generateAuthUrl({
+    access_type: "offline",
+    scope: ["https://www.googleapis.com/auth/youtubepartner",
+            "https://www.googleapis.com/auth/youtube",
+            "https://www.googleapis.com/auth/youtube.force-ssl"]
+  }));
+  // funcOut("coucou");
+};
+
+ // const spotifyApi = new SpotifyWebApi({
+ //    clientId : "",
+ //    clientSecret : "",
+ //    redirectUri : "http://www.example.com/callback"
+ //  });
+
+ //  spotifyApi.getArtistAlbums('43ZHCT0cAZBISjO8DG9PnE')
+ //  .then(function(data) {
+ //    console.log('Artist albums', data.body);
+ //  }, function(err) {
+ //    console.error(err);
+ //  });
