60793e2de5e02279d1686a5ee314b1250db704b8
balatzar <bataprod@gmail.com>
2016-05-01 09:24:54 +0200

    create models for pendu

---
+++ src/apps/pendu/models/Game.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const gameSchema = new mongoose.Schema({
+  createdAt: { type: Date, default: Date.now },
+  playing: { type: Boolean, default: true },
+  channel: String,
+  winners: [String],
+});
+
+const Game = {
+  model: mongoose.model("games", gameSchema),
+
+  createGame: function(game) {
+    "use strict";
+    Tokens.model.create(game);
+  },
+  
+};
+
+module.exports = Game;
---
+++ src/apps/pendu/models/Word.js
@@ -0,0 +1,21 @@
+const mongoose  = require("mongoose");
+const random    = require('mongoose-random');
+
+const wordSchema = new mongoose.Schema({
+  createdAt: { type: Date, default: Date.now },
+  word: String,
+});
+
+wordSchema.plugin(random, { path: 'r' });
+
+const Word = {
+  model: mongoose.model("words", wordSchema),
+
+  createWatcher: function(word) {
+    "use strict";
+    Tokens.model.create(word);
+  },
+  
+};
+
+module.exports = Word;
