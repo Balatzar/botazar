480623d8335bcdad791f55baff2f2fcfff5953f6
balatzar <bataprod@gmail.com>
2016-05-25 20:09:06 +0200

    add security

--- src/apps/mycode/modules/baseModule.js
+++ src/apps/mycode/modules/baseModule.js
@@ -1,8 +1,22 @@
-const User = require("../../../services/slack/models/User");
+const User      = require("../../../services/slack/models/User");
+const Channel   = require("../../../services/slack/models/Channel");
 
 module.exports = function(objMessage, funcOut) {
   "use strict";
-  User.model.findOne({ id: objMessage.user }, function(err, user) {
-    funcOut(user.code);
+  Channel.model.findOne({ id: objMessage.channel }, function(err, channel) {
+    if (err) {
+      console.log(err);
+    } else if (channel) {
+      funcOut("ne me demandez pas ça dans un channel publique ! :0 venez me voir en mp ;)");
+    } else {
+      User.model.findOne({ id: objMessage.user }, function(err, user) {
+        if (err) {
+          console.log(err);
+        } else {
+          funcOut(user.code);
+        }
+      });
+    }
   });
 };
+
