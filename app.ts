42f3f1d057b4f27229d1f0f761c46adbbd1f4502
balatzar <bataprod@gmail.com>
2016-05-27 09:17:51 +0200

    add ts

--- app.ts
+++ app.ts
@@ -1,3 +1,5 @@
+/// <reference path="typings/globals/node/index.d.ts" />
+
 const mongoose      = require("mongoose");
 const slackService  = require("./src/services/slack/slack");
 const webService    = require("./src/services/web/server");
