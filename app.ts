1bedcfffce1ef4e8da2ac9c0ff6090a529206df4
balatzar <bataprod@gmail.com>
2016-09-09 08:30:15 +0200

    remove ts-built folder

--- app.ts
+++ app.ts
@@ -4,7 +4,4 @@ import slackService from "./src/services/slack/slack";
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/botazar");
 console.log("Connected to mongo!");
 
-console.log(process.env.SLACK_API_TOKEN)
-
 slackService(process.env.SLACK_API_TOKEN);
-
--- ts-built/app.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-var mongoose = require("mongoose");
-var slack_1 = require("./src/services/slack/slack");
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/botazar");
-console.log("Connected to mongo!");
-console.log(process.env.SLACK_API_TOKEN);
-slack_1.default(process.env.SLACK_API_TOKEN);
